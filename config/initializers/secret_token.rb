c5098993a7382a7e43e0af60937368bd97218f20
Zak <laazzak@gmail.com>
2015-06-04 15:11:25 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bonne::Application.config.secret_token = 'bb2f901430476e037a0c938b96dfaf102e94b698d716161821ec7f6d906db62ab26f7ba557595268ebd1fcc70a40144849e561218ba194973aad7d7bfb42c472'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bonne::Application.config.session_store :cookie_store, :key => '_bonne_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bonne::Application.config.session_store :active_record_store
